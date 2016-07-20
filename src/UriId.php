54e4195ab31717b6539c9f670bc29a7a11b093d9
Michał Tatarynowicz <tatarynowicz@gmail.com>
2016-07-20 21:07:25 +0200

    Added UriId. (#16)
    * Added UriId.
    
    * Fixes after review.

---
+++ src/UriId.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Assertis\Util;
+
+use InvalidArgumentException;
+
+/**
+ * @author Michał Tatarynowicz <michal.tatarynowicz@assertis.co.uk>
+ */
+abstract class UriId extends Id
+{
+    private const VALUE_REGEX = '|^(/[a-z0-9\-_\.\?\=]+)+$|';
+
+    /**
+     * @return string
+     */
+    protected function getValueRegex()
+    {
+        return self::VALUE_REGEX;
+    }
+
+    /**
+     * @return string
+     */
+    protected function assertIsValidUri($value)
+    {
+        if (preg_match($this->getValueRegex(), $value)) {
+            return;
+        }
+
+        throw new InvalidArgumentException(sprintf(
+            '%s is not a valid value for %s',
+            $value,
+            get_class($this)
+        ));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function __construct($value)
+    {
+        $this->assertIsValidUri($value);
+
+        parent::__construct($value);
+    }
+}
