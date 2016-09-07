8f082dda0dad582fa24e17ea43cfd79a81f006fd
Rafał Orłowski <orlowski@ifelse.pl>
2016-09-07 15:13:58 +0200

    AmountPenceList class introduced

---
+++ src/AmountPenceList.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Assertis\Util;
+
+/**
+ * @author Rafał Orłowski <rafal.orlowski@assertis.co.uk>
+ */
+class AmountPenceList extends ObjectList
+{
+
+    /**
+     * Return true if the value is acceptable for this list. Typically something like:
+     *   return $value instanceof MyClass
+     *
+     * @param mixed $value
+     * @return boolean
+     */
+    public function accepts($value)
+    {
+        return $value instanceof AmountPence;
+    }
+}
---
+++ tests/AmountPenceListTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Assertis\Util;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * @author Rafał Orłowski <rafal.orlowski@assertis.co.uk>
+ */
+class AmountPenceListTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testAccept(){
+        $list = new AmountPenceList();
+        $list->append(new AmountPence(99));
+
+        $this->assertCount(1, $list);
+    }
+
+}
