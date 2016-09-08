4a70271231599cd18c2570766eb0b369748298ae
Rafał Orłowski <orlowski@ifelse.pl>
2016-09-08 15:00:57 +0200

    DateList object introduced

---
+++ src/DateList.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Assertis\Util;
+
+/**
+ * @author Rafał Orłowski <rafal.orlowski@assertis.co.uk>
+ */
+class DateList extends ObjectList
+{
+
+    /**
+     * Return true if the value is acceptable for this list. Typically something like:
+     *   return $value instanceof MyClass
+     *
+     * @param mixed $value
+     * @return boolean
+     */
+    public function accepts($value)
+    {
+        return $value instanceof Date;
+    }
+}
