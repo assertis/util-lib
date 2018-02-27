ea15f65326e90ac9492f9a64d51bf4423c58b5e9
Luke <nowak.lukasz93@gmail.com>
2018-02-27 12:17:42 +0100

    One class to rule them all (#34)
    * Common service status response
    
    * New line at the end of the file
    
    * Service => Services

---
+++ src/Jsend/ServiceStatusResponse.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Assertis\Util\Jsend;
+
+/*
+ * @author Lukasz Nowak <lukasz.nowak@assertis.co.uk>
+ */
+use Assertis\Http\Client\ClientInterface;
+use Assertis\Http\Request\Request;
+use Symfony\Component\HttpFoundation\Response as Status;
+
+class ServiceStatusResponse
+{
+
+    /**
+     * @var string
+     */
+    private $status;
+    /**
+     * @var string
+     */
+    private $name;
+    /**
+     * @var int
+     */
+    private $apiVersion;
+    /**
+     * @var array
+     */
+    private $mysql;
+    /**
+     * @var array
+     */
+    private $services;
+    /**
+     * @var array
+     */
+    private $settings;
+    /**
+     * @var array
+     */
+    private $config;
+    /**
+     * @var string
+     */
+    private $environment;
+
+    /**
+     * ServiceStatusResponse constructor.
+     * @param string $status
+     * @param string $environment
+     * @param string $name
+     * @param int $apiVersion
+     * @param array $mysql
+     * @param array $services
+     * @param array $settings
+     * @param array $config
+     */
+    public function __construct(
+        string $status,
+        string $environment,
+        string $name,
+        int $apiVersion,
+        array $mysql = [],
+        array $services = [],
+        array $settings = [],
+        array $config = []
+    ) {
+        $this->status = $status;
+        $this->environment = $environment;
+        $this->name = $name;
+        $this->apiVersion = $apiVersion;
+        $this->mysql = $mysql;
+        $this->services = $services;
+        $this->settings = $settings;
+        $this->config = $config;
+    }
+
+    public function getResponseStatus(): int
+    {
+        if (strcmp($this->status, StatusEnum::SUCCESS) === 0) {
+            return 200;
+        }
+        return 503;
+    }
+
+    public function getResponseBody(): array
+    {
+        return [
+            'status' => $this->status,
+            'data' => [
+                'name' => $this->name,
+                'environment' => $this->environment,
+                'apiVersion' => $this->apiVersion,
+                'mysql' => $this->mysql,
+                'settings' => $this->settings,
+                'services' => $this->services,
+                'config' => $this->config
+            ],
+        ];
+    }
+
+    /**
+     * @param ClientInterface $client
+     * @param string $url
+     * @return bool
+     */
+    public static function isServiceOk(ClientInterface $client, string $url = 'status'): bool
+    {
+        $response = $client->send(new Request($url, '', [], Request::GET));
+
+        if ($response->getStatusCode() !== Status::HTTP_OK) {
+            return false;
+        }
+
+        $data = $response->json();
+
+        return is_array($data) && in_array($data['status'], ['ok', 'success']);
+    }
+
+}
