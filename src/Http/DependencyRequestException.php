44851dffde9e07e8894c7d696d5ed86a1a751676
bartek <bartman3000@gmail.com>
2018-10-02 14:51:25 +0200

    Introduced DependencyRequestException

---
+++ src/Http/DependencyRequestException.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types = 1);
+
+/**
+ * @author Bartłomiej Olewiński <bartlomiej.olewinski@assertis.co.uk>
+ */
+
+namespace Assertis\Util\Http;
+
+use Assertis\Util\RequestException;
+use Symfony\Component\HttpFoundation\Response;
+
+class DependencyRequestException extends RequestException
+{
+
+    const ERROR_CODE = 'DEPENDENT-REQUEST-FAIL';
+
+    /**
+     * @return int
+     */
+    public function getHttpCode()
+    {
+        return Response::HTTP_FAILED_DEPENDENCY;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSpecificErrorCode()
+    {
+        return self::ERROR_CODE;
+    }
+}
